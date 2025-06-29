f8165a2c2296cc55085bbecb71c1c9bad97fbb13
ashokvanam <ashokmyapps@gmail.com>
2025-06-29 18:48:28 +1000

    latest changes to guatemala

--- src/js/main.js
+++ src/js/main.js
@@ -36,11 +36,23 @@ var g_wmsLayers_list = [];
 function addBaseMap() {
   window.mapRef = new Map({
     target: "map",
-    layers: [
+    layers: [      
       new TileLayer({
-        source: new OSM(),
-        title: "Open Street Map",
-        visible: true,
+       source: new TileWMS({
+        url:
+          configData.proxy?configData.WMSProxy + "?url=" +configData.WMSEndPoint +"?":configData.WMSEndPoint,
+        params: {
+          LAYERS: "basemap",
+          FORMAT: "image/png",
+          TILED: true,
+        },
+        tileLoadFunction:authTileLoadFunction
+      }),
+      title: "Basemap",
+      //minZoom: eachVisibleLayer.minZoom,
+      //maxZoom: eachVisibleLayer.maxZoom,
+      zIndex : 0
+      // visible: eachVisibleLayer.visible,
       }),
     ],
     view: new View({
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default {
     }
   },  
   server: {
-    port: 8080,
+    port: 8081,
     hot: true
   },
   build: {
